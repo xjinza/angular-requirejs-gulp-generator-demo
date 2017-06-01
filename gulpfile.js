cbfafcc36078290988bc3fce172637be0d65d193
xjinza <xjinza@hotmail.com>
2017-06-01 21:22:50 +0800

    gulp setting
    gulp

---
+++ gulpfile.js
@@ -0,0 +1,147 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var minimist = require('minimist');
+
+// load plugins
+
+var	del = require('del'),
+	imagemin = require('gulp-imagemin'),
+	minifyHtml = require('gulp-minify-html'),
+	minifyCss = require('gulp-minify-css'),
+	plumber = require('gulp-plumber'),
+	replace = require('gulp-replace'),
+	// rev = require('gulp-rev'),
+	// revReplace = require('gulp-rev-replace'),
+	runSequence = require('run-sequence'),
+	uglify = require('gulp-uglify');
+
+// parse environment
+var options = minimist(process.argv.slice(2), {
+	string: 'env',
+	default: {
+		env: 'dev'
+	}
+});
+var project = "golf_web";
+var projectDist=project+"_build";
+// get build options for the environment
+var buildOptions;
+if (options.env === 'prod') {
+	buildOptions = {
+		tasks: ['build'],
+		dirs: [projectDist]
+	};
+} else {
+	buildOptions = {
+		tasks: [],
+		dirs: [project]
+	};
+}
+
+// local server utils
+var runServer = function(open, callback) {
+	browserSync({
+		notify: false,
+		port: 9000,
+		open: open,
+		server: true
+	}, callback);
+};
+
+
+gulp.task("js", function() {
+	//JS路径操作
+	gulp.src(project+"/app/**/*.js")
+
+	.pipe(uglify())
+	.pipe(gulp.dest(projectDist+'/app'))
+
+})
+
+gulp.task("html", function() {
+	//JS路径操作
+	gulp.src([project+'/app/**/*.html'])
+	.pipe(minifyHtml())
+	.pipe(gulp.dest(projectDist+'/app'))
+
+	gulp.src([project+'/*.html'])
+	.pipe(minifyHtml())
+	.pipe(gulp.dest(projectDist))
+
+})
+gulp.task("css", function() {
+	//JS路径操作
+	gulp.src([project+'/css/*.css'])
+
+	.pipe(minifyCss())
+	.pipe(gulp.dest(projectDist+'/css'))
+
+})
+
+gulp.task('img', function() {
+	return gulp.src(project+'/img/*')
+		.pipe(imagemin({
+			progressive: true,
+			interlaced: true
+		}))
+		.pipe(gulp.dest(projectDist+'/img'));
+});
+
+
+gulp.task('extras', function() {
+	 gulp.src([project+'/plugins/**/*'], {
+			dot: true
+		})
+		.pipe(gulp.dest(projectDist+'/plugins'));
+
+	 gulp.src([project+'/WEB-INF/**/*'], {
+			dot: true
+	 })
+	.pipe(gulp.dest(projectDist+'/WEB-INF'));
+});
+
+gulp.task('clean', del.bind(null, [projectDist+'/*']));
+
+gulp.task('serve', function(done) {
+
+	var tasks = buildOptions.tasks.slice(0);
+	tasks.push('run');
+
+	if (options.env !== 'prod') {
+		tasks.push('watch');
+	}
+
+	tasks.push(done);
+
+	runSequence.apply(null, tasks);
+});
+
+gulp.task('run', function(done) {
+	runServer(true, done);
+});
+
+gulp.task('watch', function() {
+	// watch for changes
+	gulp.watch([
+		project+'/app/**/*.html',
+		project+'/*.html',
+		project+'/app/**/*.js',
+		project+'/img/*',
+		project+'/css/*'
+	]).on('change', reload);
+
+});
+
+
+gulp.task('build', function(done) {
+	runSequence('clean','html', 'img', 'css', 'js', 'extras',  done);
+	
+});
+
+gulp.task('default', function(done) {
+	runSequence('serve',  done);
+});
